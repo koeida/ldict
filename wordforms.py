cd1bface7f0d1cc3f8dcedc846ee05b1d189bfd3
koeida <koeida@github.com>
2020-01-29 08:57:04 -0500

    blork

--- wordforms.py
+++ wordforms.py
@@ -1,12 +1,12 @@
 import sys
-import regex
-from copy import copy
-from functools import reduce
-from jellyfish import jaro_winkler
+import regex #type: ignore
+from copy import copy #type: ignore
+from functools import reduce #type: ignore
+from jellyfish import jaro_winkler #type: ignore
+from typing import Callable, List, Dict, Iterator
 
-
-def count_unique(words):
-    results = {}
+def count_unique(words: List[str]) -> Dict[str, int]:
+    results = {} # type: Dict[str,int]
     for w in words:
         if w in results:
             results[w] += 1
@@ -14,8 +14,8 @@ def count_unique(words):
             results[w] = 1
     return results
 
-def partition(f,l):
-    r1 = list(filter(f,l))
+def partition(f, l):
+    r1 = list(filter(f, l))
     r2 = list(filter(lambda x: not f(x), l))
     return r1,r2
 
@@ -32,37 +32,38 @@ def merge_dicts(d1,d2,f):
             merged[k] = d1[k]
     return merged
 
-def to_clean_words(s):
-    results = s.replace("...", " ")
-    results = results.replace("-", "")
-    results = regex.sub("[\.,!:\"'`\?]","",results)
-    results = results.split()
-    results = map(lambda w: w.upper(), results)
-    results = sorted(list(results))
-    return results
+def to_clean_words(s: str) -> List[str]:
+    s = s.replace("...", " ")
+    s = s.replace("-", "")
+    s = regex.sub("[\*\.,!:\"'`\?]","", s)
+    results = list(s.split())
+    results = list(filter(lambda x: x.strip() != "", results))
+    results = list(map(lambda w: w.upper(), results))
+    return sorted(results)
 
-def get_word_usage_counts(s):
+def get_word_usage_counts(s: str) -> Dict[str,int]:
     words = to_clean_words(s)
-    words = count_unique(words)
-    return words
+    return count_unique(words)
 
-def is_similar_enough(w,ws):
+def is_similar_enough(w: str, ws: List[str]) -> bool:
     min_similarity = 0.9
     for r in ws:
         s = jaro_winkler(w,r)
-        if s >= min_similarity and r[0] == w[0]:
+        are_similar = (s >= min_similarity and r[0] == w[0])
+        start_with_same_letters = r[:4] == w[:4]
+        if are_similar: # or start_with_same_letters:
             return True
     return False
 
 
-def to_similarity_list(words):
-    results = []
+def to_similarity_list(words: List[str]) -> List[str]:
+    results = [] #type: List[str]
     for w in words:
         if not is_similar_enough(w,results):
             results.append(w)
     return results
 
-def paragraphs(s):
+def paragraphs(s: str) -> List[str]:
     paragraph_split = "\n"
     paragraphs = regex.split(paragraph_split, s)
     return list(filter(lambda x: x.strip() != "", paragraphs))
@@ -89,9 +90,9 @@ last_paragraph_usages = get_word_usage_counts(last_paragraph_text)
 only_end_words = dictionary_intersect(word_usages, last_paragraph_usages)
 endmulti, endsingle = partition(lambda kv: kv[1] > 1, only_end_words.items())
 endsingle = list(map(lambda x: x[0], endsingle))
-end_similar, end_newfangled = partition(lambda w: is_similar_enough(w, most_usages), endsingle)
+end_similar, end_newfangled = partition(lambda w: is_similar_enough(w, list(most_usages.keys())), endsingle)
 
-similar_words = to_similarity_list(word_usages.keys())
+similar_words = to_similarity_list(list(word_usages.keys()))
 total_similar_words = len(similar_words)
 
 # Output results
